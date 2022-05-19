ad260052822968aea8575f58af80ee75bd65411f
Mahima Bhardwaj <bhardwajmahima81@gmail.com>
2022-05-19 22:53:58 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+// program to check whether the number is even or odd//
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+ system("cls");
+ int n;
+   std:: cout<<"enter the value of n:" ;
+   std::cin>>n;
+   if(n%2==0)
+    
+         std::cout<<"the number is even:";
+       else
+          std:: cout<<"the number is odd:" ;
+          return 0;
+       
+
+}
