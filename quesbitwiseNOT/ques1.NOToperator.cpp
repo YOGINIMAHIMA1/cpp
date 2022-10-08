ae7a76c2629a975b8c4fbe6ad73ae655cfb7e17e
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-08 08:09:34 -0700

    Create ques1.NOToperator.cpp

---
+++ quesbitwiseNOT/ques1.NOToperator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a= 10;
+   int b=22;
+   int c= -25;
+   int d= -45;
+  int e=34;
+  int f= -50;
+  
+  cout<<a<<endl;
+  cout<<b<<endl;
+  cout<<c<<endl;
+  cout<<d<<endl;
+  cout<<e<<endl;
+  cout<<f<<endl;
+  
+  return 0;
+}
