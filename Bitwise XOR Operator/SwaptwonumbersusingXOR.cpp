1a2061b10f374678b1d20e7cb3bf115c9f8f8560
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-08 05:18:30 -0700

    Create SwaptwonumbersusingXOR.cpp
    Swapping of two numbers using XOR operator.

---
+++ Bitwise XOR Operator/SwaptwonumbersusingXOR.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+  int a=9;
+  int b=18;
+   a =  a^b;
+  b =  a^b;
+  a =  a^b;
+  
+  cout<<a<<endl;
+  cout<<b<<endl;
+  return 0;
+}
+  
