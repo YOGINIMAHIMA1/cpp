258c3493d3143096a12e73bd6435504ba961ccc8
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-11 09:00:10 -0700

    Create class1.cpp

---
+++ classand object/class1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Box
+{
+  int BoxNumber;
+  float Side;
+  float Area;
+  void ExecArea()
+  {
+    Area= Side*Side;
+  }
+  public:
+  void GetBox()
+  {
+    cout<<"Enter the value of BoxNumber :"<<endl;
+    cin>>BoxNumber;
+    cout<<"Enter the value of Side :"<<endl;
+    cin>>Side;
+    Execarea();
+  }
+  void ShowBox()
+  {
+    cout<<"BoxNumber :"<<endl;
+    cin>>BoxNumber ;
+    cout<<"Side :"<<endl;
+    cin>>Side;
+    cout<<"Area :"<<endl;
+    cin>>Area ;
+  };
+  int main()
+  {
+    Box.B;
+    B.GetBox();
+    B.ShowBox();
+    return 0;
+  }
