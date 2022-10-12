ebc024f2ada326f566aaaa19fa204501cdc3b9e5
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-10-11 19:25:27 -0700

    Create Instancevariable.cpp

---
+++ variables/Instancevariable.cpp
@@ -0,0 +1,30 @@
+/*
+-They are declared inside class but outside the function,block or methods.
+-It is not neccessary to intialize them. because memory is allocated to them only when object is created and they are destroyed when object is destroyed.
+-by default access specifier is private. */
+#include <iostream>
+using namespace std;
+class Box
+{
+  int BoxNumber;
+  public:
+  void GetBox()
+  {
+    int BoxShape=19 ;
+    cout<<"boxshape :"<<endl;
+    cin>>BoxShape;
+  }
+  void ShowBox()
+  {
+    cout<<"Enter BoxNumber:"<<endl;
+    cin>>BoxNumber ;
+  }
+};
+int main()
+{
+  Box B;
+  B.GetBox();
+  B.ShowBox();
+  return 0;
+}
+  
